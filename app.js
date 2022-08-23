68a20e41a9f31225b039dc71f54003722745cd27
AdetunjiJakes <68664742+Jamleads@users.noreply.github.com>
2022-08-23 09:13:45 +0100

    My dummy portfolio website
    My dummy portfolio website

---
+++ app.js
@@ -0,0 +1,13 @@
+// talking about varriables only 
+var user;   //uninitialized Variables
+user = 20; 
+
+var person; //uninitialized
+
+
+// nameing  variables
+var _name
+var Name 
+var name
+
+console.log (user)
